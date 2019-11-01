445d35c09ecb1d3befe96fd949add8d890b9969e
Pham Hanh <48047723+Joypham@users.noreply.github.com>
2019-11-01 15:59:57 +0700

    Update filter combination in combination.sql

--- filter combination in combination.sql
+++ filter combination in combination.sql
@@ -8,3 +8,17 @@ where joy_checking is not null)
 Select * from t1
 Join t2 on t1.Combination_name like CONCAT ('%',t2.Combination_name_band,'%')
 ORDER BY t1.Combination_name
+
+
+*Joy write
+
+with t1 as (
+select uuid, Combination_name from artist_combination_joy_06082019
+where joy_checking is null),
+ t2 as (
+select uuid as uuid_band, Combination_name as Combination_name_band from artist_combination_joy_06082019
+where joy_checking like '%groupname%'
+)
+Select * from t1
+Join t2 on t1.Combination_name like CONCAT ('%',t2.Combination_name_band,'%')
+ORDER BY t1.Combination_name
